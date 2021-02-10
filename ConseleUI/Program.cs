f97111b8ce2617e4163ae9d3a17053ac44873578
Veysel Karam YILMAZ <veyselkaramyilmaz@gmail.com>
2021-02-11 00:32:00 +0300

    11.02.2021

--- ConseleUI/Program.cs
+++ ConseleUI/Program.cs
@@ -11,15 +11,42 @@ namespace ConseleUI
     {
         static void Main(string[] args)
         {
+            //Data Transformation Object
+            ProductTest();
+            //CategoryTest();
+        }
+
+        private static void CategoryTest()
+        {
+            CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+            foreach (var category in categoryManager.GetAll())
+            {
+                Console.WriteLine(category.CategoryName);
+            }
+        }
+
+        private static void ProductTest()
+        {
             ProductManager productManager = new ProductManager(new EfProductDal());
 
-            foreach (var product in productManager.GetByUnitPrice(40,100))
+            var result = productManager.GetProductDetails();
+
+            if (result.Success == true)
             {
-                Console.WriteLine(product.ProductName);
-                    
+
+                foreach (var product in result.Data)
+                {
+                    Console.WriteLine(product.ProductName + "/" + product.CategoryName);
+
+                }
+            }
+            else
+            {
+                Console.WriteLine(result.Message);
+            }
             }
 
-           
+
+            
         }
     }
-}
